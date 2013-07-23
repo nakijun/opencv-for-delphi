c4d13d764c17decc41eb02d732fd4bfa524af1d4
pascal.ilya@gmail.com <pascal.ilya@gmail.com@aadbbc1a-59f3-7616-849c-30cf810a916f>
2013-07-23 10:47:58 +0000

    Data sampling

--- cv.pas
+++ cv.pas
@@ -129,6 +129,22 @@ const
   CV_WARP_FILL_OUTLIERS = 8;
   CV_WARP_INVERSE_MAP   = 16;
 
+  CV_MOP_ERODE    = 0;
+  CV_MOP_DILATE   = 1;
+  CV_MOP_OPEN     = 2;
+  CV_MOP_CLOSE    = 3;
+  CV_MOP_GRADIENT = 4;
+  CV_MOP_TOPHAT   = 5;
+  CV_MOP_BLACKHAT = 6;
+
+  { Methods for comparing two array }
+  CV_TM_SQDIFF        = 0;
+  CV_TM_SQDIFF_NORMED = 1;
+  CV_TM_CCORR         = 2;
+  CV_TM_CCORR_NORMED  = 3;
+  CV_TM_CCOEFF        = 4;
+  CV_TM_CCOEFF_NORMED = 5;
+
 (****************************************************************************************\
 *                                    Image Processing                                    *
 \****************************************************************************************)
@@ -193,7 +209,7 @@ procedure cvCvtColor(const src: PCvArr; dst: PCvArr; code: Integer); cdecl;
 procedure cvResize(const src: PCvArr; dst: PCvArr; interpolation: Integer = CV_INTER_LINEAR); cdecl;
 
 { Warps image with affine transform }
-{! Variable flags have to default value is CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS !}
+{! Variable flags have to have default value is CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS !}
 procedure cvWarpAffine(const src: PCvArr; dst: PCvArr; const map_matrix: PCvMat;
    flags: Integer; fillval: TCvScalar); cdecl;
 
@@ -206,7 +222,7 @@ function cv2DRotationMatrix(center: TCvPoint2D32f; angle: Double;
    scale: Double; map_matrix: PCvMat): PCvMat; cdecl;
 
 { Warps image with perspective (projective) transform }
-{! Variable flags have to default value is CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS !}
+{! Variable flags have to have default value is CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS !}
 procedure cvWarpPerspective(const src: PCvArr; dst: PCvArr; const map_matrix: PCvMat;
    flags: Integer; fillval: TCvScalar); cdecl;
 
@@ -215,7 +231,7 @@ function cvGetPerspectiveTransform(const src: PCvPoint2D32f; const dst: PCvPoint
    map_matrix: PCvMat): PCvMat; cdecl;
 
 { Performs generic geometric transformation using the specified coordinate maps }
-{! Variable flags have to default value is CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS !}
+{! Variable flags have to have default value is CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS !}
 procedure cvRemap(const src: PCvArr; dst: PCvArr; const mapx: PCvArr; const mapy: PCvArr;
    flags: Integer; fillval: TCvScalar); cdecl;
 
@@ -232,9 +248,12 @@ procedure cvLinearPolar(const src: PCvArr; dst: PCvArr;
    center: TCvPoint2D32f; maxRadius: Double;
    flags: Integer = (CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS)); cdecl;
 
-{ creates structuring element used for morphological operations }
-function cvCreateStructuringElementEx(cols: Integer; rows: Integer; anchor_x: Integer; anchor_y: Integer;
-            shape: Integer; values: PInteger = nil): PIplConvKernel; cdecl;
+{ creates structuring element used for morphological operations }
+function cvCreateStructuringElementEx(cols, rows: Integer; anchor_x, anchor_y: Integer;
+   shape: Integer; values: PInteger = nil): PIplConvKernel; cdecl;
+
+{ releases structuring element }
+procedure cvReleaseStructuringElement(element: P2PIplConvKernel); cdecl;
 
 { erodes input image (applies minimum filter) one or more times.
    If element pointer is NULL, 3x3 rectangular element is used }
@@ -246,8 +265,49 @@ procedure cvErode(const src: PCvArr; dst: PCvArr; element: PIplConvKernel = nil;
 procedure cvDilate(const src: PCvArr; dst: PCvArr; element: PIplConvKernel = nil;
   iterations: Integer = 1); cdecl;
 
-{ releases structuring element }
-procedure cvReleaseStructuringElement(element: P2PIplConvKernel); cdecl;
+{ Performs complex morphological transformation }
+procedure cvMorphologyEx(const src: PCvArr; dst: PCvArr; temp: PCvArr; element: PIplConvKernel;
+   operation: Integer; iterations: Integer = 1); cdecl;
+
+{ Calculates all spatial and central moments up to the 3rd order }
+procedure cvMoments(const arr: PCvArr; moments: PCvMoments; binary: Integer = 0); cdecl;
+
+{ Retrieve particular spatial, central or normalized central moments }
+function cvGetSpatialMoment(moments: PCvMoments; x_order, y_order: Integer): Double; cdecl;
+function cvGetCentralMoment(moments: PCvMoments; x_order, y_order: Integer): Double; cdecl;
+function cvGetNormalizedCentralMoment(moments: PCvMoments; x_order, y_order: Integer): Double; cdecl;
+
+{ Calculates 7 Hu's invariants from precalculated spatial and central moments }
+procedure cvGetHuMoments(moments: PCvMoments; hu_moments: PCvHuMoments); cdecl;
+
+(*********************************** data sampling **************************************)
+
+{ Fetches pixels that belong to the specified line segment and stores them to the buffer.
+   Returns the number of retrieved points. }
+function cvSampleLine(const image: PCvArr; pt1, pt2: TCvPoint; buffer: Pointer;
+   connectivity: Integer = 8): Integer; cdecl;
+
+{ Retrieves the rectangular image region with specified center from the input array.
+ dst(x,y) <- src(x + center.x - dst_width/2, y + center.y - dst_height/2).
+ Values of pixels with fractional coordinates are retrieved using bilinear interpolation }
+procedure cvGetRectSubPix(const src: PCvArr; dst: PCvArr; center: TCvPoint2D32f); cdecl;
+
+{ Retrieves quadrangle from the input array.
+    matrixarr = ( a11  a12 | b1 )   dst(x,y) <- src(A[x y]' + b)
+                ( a21  a22 | b2 )   (bilinear interpolation is used to retrieve pixels
+                                     with fractional coordinates)
+}
+procedure cvGetQuadrangleSubPix(const src: PCvArr; dst: PCvArr; const map_matrix: PCvMat); cdecl;
+
+{ Measures similarity between template and overlapped windows in the source image
+   and fills the resultant image with the measurements }
+procedure cvMatchTemplate(const image: PCvArr; const templ: PCvArr; result: PCvArr; method: Integer); cdecl;
+
+{ Computes earth mover distance between
+   two weighted point sets (called signatures) }
+function cvCalcEMD2(const signature1, signature2: PCvArr; distance_type: Integer;
+   distance_func: TCvDistanceFunction = nil; const cost_matrix: PCvArr = nil;
+   flow: PCvArr = nil; lower_bound: PSingle = nil; userdata: Pointer = nil): Single; cdecl;
 
 { Runs canny edge detector }
 procedure cvCanny(const image: PCvArr; edges: PCvArr; threshold1, threshold2: Double;
@@ -282,12 +342,25 @@ procedure cvRemap; external CV_DLL name 'cvRemap';
 procedure cvConvertMaps; external CV_DLL name 'cvConvertMaps';
 procedure cvLogPolar; external CV_DLL name 'cvLogPolar';
 procedure cvLinearPolar; external CV_DLL name 'cvLinearPolar';
-
-procedure cvEqualizeHist; external CV_DLL name 'cvEqualizeHist';
 function cvCreateStructuringElementEx; external CV_DLL name 'cvCreateStructuringElementEx';
+procedure cvReleaseStructuringElement; external CV_DLL name 'cvReleaseStructuringElement';
 procedure cvErode; external CV_DLL name 'cvErode';
 procedure cvDilate; external CV_DLL name 'cvDilate';
-procedure cvReleaseStructuringElement; external CV_DLL name 'cvReleaseStructuringElement';
+procedure cvMorphologyEx; external CV_DLL name 'cvMorphologyEx';
+procedure cvMoments; external CV_DLL name 'cvMoments';
+function cvGetSpatialMoment; external CV_DLL name 'cvGetSpatialMoment';
+function cvGetCentralMoment; external CV_DLL name 'cvGetCentralMoment';
+function cvGetNormalizedCentralMoment; external CV_DLL name 'cvGetNormalizedCentralMoment';
+procedure cvGetHuMoments; external CV_DLL name 'cvGetHuMoments';
+procedure cvGetQuadrangleSubPix; external CV_DLL name 'cvGetQuadrangleSubPix';
+procedure cvMatchTemplate; external CV_DLL name 'cvMatchTemplate';
+function cvCalcEMD2; external CV_DLL name 'cvCalcEMD2';
+
+{ data sampling }
+function cvSampleLine; external CV_DLL name 'cvSampleLine';
+procedure cvGetRectSubPix; external CV_DLL name 'cvGetRectSubPix';
+
+procedure cvEqualizeHist; external CV_DLL name 'cvEqualizeHist';
 procedure cvCanny; external CV_DLL name 'cvCanny';
 
 end.
